32a2bcb2b6298494b0fffdb6ca45eb9b9504e252
emonShimoul <emonshimoul@gmail.com>
2022-06-27 15:37:34 +0600

    practice to connect with react client and mongodb

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const { json } = require('express/lib/response');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+// convert the string data to json
+app.use(express.json());
+
+const users = [
+    {id: 0, name: 'Shabana', email: 'Shabana@gmail.com', phone: '01788888888'},
+    {id: 1, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '01788888888'},
+    {id: 2, name: 'Srabonti', email: 'Srabonti@gmail.com', phone: '01788888888'},
+    {id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '01788888888'},
+    {id: 4, name: 'Soniya', email: 'Soniya@gmail.com', phone: '01788888888'},
+    {id: 5, name: 'Sushmita', email: 'Sushmita@gmail.com', phone: '01788888888'},
+    {id: 6, name: 'Monika', email: 'Monika@gmail.com', phone: '01788888888'},
+]
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mydbuser1:5GTBgBhpP8tye52q@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("myDb");
+      const userCollection = database.collection("users");
+
+      console.log("hitting the database");
+
+      // POST API
+      app.post('/users', async(req, res) => {
+          const newUser = req.body;
+          const result = await userCollection.insertOne(newUser);
+          console.log(`A document was inserted with the _id: ${result.insertedId}`);
+          res.json(result);
+      });
+
+      // GET API
+      app.get('/users', async(req, res) => {
+          const cursor = userCollection.find({});
+          const users = cursor.toArray();
+          res.send(users);
+      })
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// app.get('/users', (req, res) => {
+//     res.send(users);
+// });
+
+app.listen(port, () => {
+    console.log('Listening on port ', port);
+})
